6fb4f49917d640ff4896c53bb081b705b4342ffb
weskhel <weskhel@gmail.com>
2022-11-12 10:38:06 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+module aws_wordpress {
+    source              = "./modules/latest"
+    database_name           = "wordpress_db"
+    database_user           = "wordpress_user"
+    database_password = "ASenhaSuperSegura"
+    shared_credentials_file = "~/.aws/credentials"
+    region                  = "us-east-2" 
+    AZ1          = "us-east-2a"
+    AZ2          = "us-east-2b"
+    AZ3          = "us-east-2c"
+    VPC_cidr     = "10.0.0.0/16"
+    subnet1_cidr = "10.0.1.0/24"  
+    subnet2_cidr = "10.0.2.0/24" 
+    subnet3_cidr = "10.0.3.0/24" 
+    PUBLIC_KEY_PATH  = "./mykey-pair.pub"
+    PRIV_KEY_PATH    = "./mykey-pair"
+    instance_type    = "t2.micro"
+    instance_class   = "db.t2.micro"
+    root_volume_size = 15
+}
